f153d9fbd2dc5e358ef39f11ddcb932d44d01ddd
saish24 <saish112@gmail.com>
2024-05-19 12:06:17 +0530

    channels

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/saish24/greetings"
+	"time"
 )
 
 func main() {
-	if str, err := greetings.RandomQuote(); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(str)
+	ch := make(chan int, 30)
+	go sendInTwoSeconds(ch)
+	go receiveInThreeSeconds(ch)
+	time.Sleep(20 * time.Second)
+	close(ch)
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
+
+func sendInTwoSeconds(channel chan<- int) {
+	for i := 0; i < 10; i++ {
+		channel <- i
+		fmt.Println("Sending ", i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func receiveInThreeSeconds(channel <-chan int) {
+	for i := 0; i < 10; i++ {
+		j := <-channel
+		fmt.Println("Received ", j)
+		time.Sleep(3 * time.Second)
 	}
 }
